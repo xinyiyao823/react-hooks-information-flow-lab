46c0e7eb84f92a7b30c36988d26528f2533cfb4d
+ <+>
2021-09-24 11:24:55 -0500

    commit tree c0b2e31683d1f1471151a30ca477749c8c4fd993

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 
 
-function Filter(changeCategory) {
+function Filter({onCategoryChange}) {
     return (
-        <select name="filter" onChange={changeCategory}>
+        <select name="filter" onChange={(e) => onCategoryChange(e.target.value)}>
             <option value="All">Filter by category</option>
             <option value="Produce">Produce</option>
             <option value="Dairy">Dairy</option>
             <option value="Dessert">Dessert</option> 
         </select>
     )
+
 }
 
 export default Filter;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -6,7 +6,7 @@ import Item from "./Item"
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function onChangeCategory() {
+  
     const itemsToDisplay = items.filter((item) => {
       if (selectedCategory === "All") {
         return true;
@@ -14,21 +14,20 @@ function ShoppingList({ items }) {
         return item.category === selectedCategory;
       }
   })   
-  }
-  
+ 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-          <Filter changeCategory={onChangeCategory}/>
+          <Filter onCategoryChange={setSelectedCategory}/>
       </div>
       <ul className="Items">
-        {/* {itemsToDisplay.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item 
           key={item.id} 
           name={item.name} 
           category={item.category} 
           />
-        ))} */}
+        ))}
       </ul>
     </div>
   );
